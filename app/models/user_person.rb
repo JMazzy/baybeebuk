f8af424f4d43aadded7cefe2886e44916364640e
Joshua Masland-Sarani <j.masland@gmail.com>
2016-07-13 15:55:41 -0400

    seeds

--- app/models/user_person.rb
+++
@@ -1,2 +0,0 @@
-class UserPerson < ApplicationRecord
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+person = Person.create(
+  first_name: "Foo",
+  middle_name: "Theodore",
+  last_name: "Bar",
+  birth_date: Date.new(1985,6,7),
+  birth_weight: 8.0 + 6.0/16,
+  birth_place: "New York, NY",
+  gender: "M")
+
+user = User.create(
+  email: "foobar@example.com",
+  password: "foo1bar2",
+  person_id: person.id )
+
+little_jimmy = Person.create(
+  first_name: "Jimmy",
+  middle_name: "Foo",
+  last_name: "Bar",
+  birth_date: Date.new(2014,5,6),
+  birth_weight: 10.0,
+  birth_place: "Seattle, WA",
+  gender: "M")
+
+little_suzie = Person.create(
+  first_name: "Suzie",
+  middle_name: "Fooie",
+  last_name: "Bar",
+  birth_date: Date.new(2016,7,8),
+  birth_weight: 7.0,
+  birth_place: "Austin, TX",
+  gender: "F")
+
+Relationship.create(parent_id: person.id, child_id: little_suzie.id )
+Relationship.create(parent_id: person.id, child_id: little_jimmy.id )
+
+Memory.create(
+  user_id: user.id,
+  person_id: little_jimmy.id,
+  title: "Tooth knocked out",
+  body: "Little Jimmy fell off his bike and his front teeth came out.",
+  memory_date: Date.new(2016,9,10) )
+
+Memory.create(
+  user_id: user.id,
+  person_id: little_suzie.id,
+  title: "Suzie turned over",
+  body: "Suzie turned herself over for the first time.",
+  memory_date: Date.new(2017,2,10) )
